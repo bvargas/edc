91c40790020e8c918963169fd90321aa6a44b9f5
belia <lauravargas1012@gmail.com>
2015-12-01 13:40:08 -0800

    dec01-second add

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 /*js goes here*/
+var socialMedia = {
+	facebook: 'https://www.facebook.com',
+	envelope: 'https://www.gmail.com',
+	linkedin: 'https://www.linkedin.com/',
+	github: 'https://github.com/',
+	slack: 'https://slack.com/'
+};
+
+var social = function(){
+	var output = '<ul>';
+	var smiList = document.querySelectorAll('.smi');
+	for(var key in arguments[0]){
+		output += '<li><a href="' + socialMedia[key] + '" target="_blank"><i class="fa fa-' + key + '"></i></a></li>';
+	}
+	output += '</ul>';
+	for(var i = 0; i < smiList.length; i++){
+		smiList[i].innerHTML = output;
+	}	
+}(socialMedia);
