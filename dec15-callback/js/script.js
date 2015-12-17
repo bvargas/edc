4bcfd5b5eb80b75f4be19876e02bcc6e7f90881f
belia <lauravargas1012@gmail.com>
2015-12-17 15:47:50 -0800

    final add

---
+++ dec15-callback/js/script.js
@@ -0,0 +1,23 @@
+/*js goes here*/
+
+var mainObj = document.getElementById('main');
+var colors = ['green', 'yellow', 'orange', 'red', 'purple', 'blue'];
+var apps = ['phone', 'notes', 'calendar', 'music', 'itunes', 'weather'];
+
+var appendElement = function(el){
+	mainObj.appendChild(el);
+	// addAppName(newDiv, i);
+	el.innerHTML = addAppName(i);
+};
+
+var addAppName = function(i){
+	// el.innerHTML = apps[i];
+	return apps[i];
+};
+
+for(var i = 0; i < colors.length; i++){
+	var newDiv = document.createElement('div');
+	newDiv.className = 'div-' + colors[i];
+
+	appendElement(newDiv);	
+}
---
+++ dec17-calendar/js/script.js
@@ -0,0 +1,152 @@
+/*js goes here*/
+
+var cal = document.querySelector('#cal .content');
+var d = new Date();
+var modM = d.getMonth();
+
+var prevBtn = document.getElementById('prev').addEventListener('click', function(evt){
+	modM--;
+	modD(evt, modM);
+	// console.log(modM);
+});
+var nextBtn = document.getElementById('next').addEventListener('click', function(evt){
+	modM++;
+	modD(evt, modM);
+	// console.log(modM);
+});
+
+var modD = function(){
+	arguments[0].preventDefault();
+	calcCal(new Date(d.getFullYear(), arguments[1], d.getDate()));
+};
+
+var calcCal = function(){
+	var $date = arguments[0];
+
+	var thisWD = $date.getDay(),
+		thisD = $date.getDate(),
+		thisM = $date.getMonth(),
+		thisY = $date.getFullYear();
+
+		cal.innerHTML = printCal(thisY, strMonth(thisM), dayOfWeek(thisY, thisM), daysInMonth(thisY, thisM), thisD);
+		// daysInMonth(thisY, thisM);
+};
+
+var today = function(year, month){
+	return new Date(year, month).getDate();
+};
+
+var dayOfWeek = function(year, month){
+	return new Date(year, month).getDay();
+};
+
+var daysInMonth = function(year, month){
+	// console.log(new Date(year, month, 32).getDate());
+	return 32 - new Date(year, month, 32).getDate();
+};
+
+
+var strMonth = function(m){
+	switch(m){
+		case 0: 
+			m = 'January';
+			break;
+		case 1:
+			m = 'February';
+			break;
+		case 2:
+			m = 'March';
+			break;
+		case 3:
+			m = 'April';
+			break;
+		case 4:
+			m = 'May';
+			break;
+		case 5:
+			m = 'June';
+			break;
+		case 6:
+			m = 'July';
+			break;
+		case 7:
+			m = 'August';
+			break;
+		case 8:
+			m = 'September';
+			break;
+		case 9:
+			m = 'October';
+			break;
+		case 10:
+			m = 'November';
+			break;
+		case 11:
+			m = 'December';
+			break;
+	}
+	return m;
+};
+var strWeekday = function(wd){
+	switch(wd){
+		case 0: 
+			wd = 'Sunday';
+			break;
+		case 1: 
+			wd = 'Monday';
+			break;
+		case 2: 
+			wd = 'Tuesday';
+			break;
+		case 3: 
+			wd = 'Wednesday';
+			break;
+		case 4: 
+			wd = 'Thursday';
+			break;
+		case 5: 
+			wd = 'Friday';
+			break;
+		case 6: 
+			wd = 'Saturday';
+			break;
+	}
+	return wd;
+};
+
+
+var printCal = function(year, month, dayOfWeek, days, today){
+	console.log(today);
+	var output =
+		'<div class="header">'+
+			'<h1>'+ month + ' ' + year +'</h1>'+
+		'</div>';
+	output += 
+		'<div class="daysOfWeek">'+
+			'<span>Sun</span>'+
+			'<span>Mon</span>'+
+			'<span>Tue</span>'+
+			'<span>Wed</span>'+
+			'<span>Thu</span>'+
+			'<span>Fri</span>'+
+			'<span>Sat</span>'+
+		'</div>';
+		output += '<div class="days">'
+	for(var h = 0; h < dayOfWeek; h++){
+		output += '<div></div>';
+	}
+	for(var i = 0; i < days; i++){
+		if(i+1 === today && d.getMonth() === modM){
+			output += '<div class="today">' + (i + 1) + '</div>'
+		}else{
+		output += '<div>' + (i + 1) + '</div>';
+		}
+	}
+	output += '</div>';
+	return output;
+};
+
+
+var initCal = function(){
+	calcCal(d);
+}(d);
